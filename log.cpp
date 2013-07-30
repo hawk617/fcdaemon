f0177c03c8db2bcf4f71220d1ab6d6f105c6715c
hawk617 <piputilov@gmail.com>
2013-07-30 11:31:03 +0400

    added timstamp to logfile

--- log.cpp
+++ log.cpp
@@ -6,6 +6,7 @@
  */
 #include <stdio.h>
 #include <string>
+#include <time.h>
 #include "log.h"
 
 using namespace std;
@@ -14,8 +15,17 @@ static FILE* log_file=0;
 
 void WriteLog(string Msg)
 {
+	time_t t;
+	struct tm *t_m;
+	t=time(NULL);
+	t_m=localtime(&t);
+	//cout<<"Local time is: "<<t_m->tm_hour<<":"<<t_m->tm_min<<":"<<t_m->tm_sec;
+
 	if (log_file==0) log_file=fopen(LOG_FILE, "a");
+	fprintf(log_file, "[%2d-%2d-%4d %2d:%2d:%2d]", t_m->tm_mday, t_m->tm_mon,
+			t_m->tm_year, t_m->tm_hour, t_m->tm_min, t_m->tm_sec);
 	fprintf(log_file, Msg.c_str());
+	delete t_m;
 }
 
 void SetLogFile(const char* Filename)
